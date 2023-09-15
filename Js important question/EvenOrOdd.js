10093025d89ca85b7c7ed06180cb0b76721aaf00
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-15 06:13:10 +0000

    commit

---
+++ Js important question/EvenOrOdd.js
@@ -0,0 +1,10 @@
+number = Number(prompt("Enter the number: "));
+
+if (number > 0){
+    if(number % 2 == 0){
+        console.log("Even number");
+    }
+    else{
+        console.log("Odd number");
+    }
+}
